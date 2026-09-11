f41bc4ccffcef5f638329de2da3664908d9cd404
datesorder <datesorder4@gmail.com>
2026-09-11 15:21:31 +0300

    Delete src/src directory

--- src/src/lib/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ieyvsbpzqzabvvetshre.supabase.co'
-const supabasePublishableKey = 'sb_publishable_yzO0Fk2ZF_o6xMGG2TAPtQ_VlXpf7oI'
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabasePublishableKey
-)
