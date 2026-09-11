3f568fca5d616284c3451862b4d16427a0491801
datesorder <datesorder4@gmail.com>
2026-09-11 14:24:56 +0300

    Update supabaseClient.js

--- src/src/lib/supabaseClient.js
+++ src/src/lib/supabaseClient.js
@@ -1,1 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'ieyvsbpzqzabvvetshre'
+const supabasePublishableKey = 'sb_publishable_yzO0Fk2ZF_o6xMGG2TAPtQ_VlXpf7oI'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabasePublishableKey
+)
